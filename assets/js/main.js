c83da25f7eb0ad97c17151497d80cb46aef95cf2
turchak <turchaksv@gmail.com>
2017-08-31 18:35:53 +0300

    fixed tab border-bottom

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ let tab = document.querySelectorAll('.menu__item');
 
 tab.forEach((element, index) => {
   tab[index].onclick = function() {
+    tab.forEach((k) => {
+      k.classList.remove('active');
+    })
+    this.className += ' active';
     description.forEach((j) => {
       j.classList.remove('show');
     })
